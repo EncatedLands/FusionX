0b1fe8fed536b0083e948d3228d87dc26a8060b1
EncatedLands <65440395+EncatedLands@users.noreply.github.com>
2020-05-17 18:03:05 +0800

    Update VersionInfo.php

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -32,7 +32,7 @@ if(defined('pocketmine\_VERSION_INFO_INCLUDED')){
 }
 const _VERSION_INFO_INCLUDED = true;
 
-const NAME = "PocketMine-MP";
+const NAME = "FusionX";
 const BASE_VERSION = "3.12.1";
 const IS_DEVELOPMENT_BUILD = false;
 const BUILD_NUMBER = 0;
